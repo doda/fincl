be14adadba1500ed35a2f924c7bee2fac14b6818
Domink Dario <dominik@doda.co>
2020-02-02 21:00:25 -0500

    Updates

---
+++ precache-manifest.c4d6dae40393114c20526f7d6421ab17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87464e32ba0063b98dfa30f6916ea27d",
+    "url": "/fincl/index.html"
+  },
+  {
+    "revision": "4dea9000849b84605922",
+    "url": "/fincl/static/css/main.d8812348.chunk.css"
+  },
+  {
+    "revision": "a8db99739dfca6686d30",
+    "url": "/fincl/static/js/2.8332bf77.chunk.js"
+  },
+  {
+    "revision": "df5eb433785d6ffadbb3718680c5d880",
+    "url": "/fincl/static/js/2.8332bf77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4dea9000849b84605922",
+    "url": "/fincl/static/js/main.c219f678.chunk.js"
+  },
+  {
+    "revision": "e0ce4ac85afdf5fa2b08",
+    "url": "/fincl/static/js/runtime-main.16b4b226.js"
+  }
+]);
